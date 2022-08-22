1dc481dd65baea66db1501b233e6282bf37d74b5
krsnadas04 <94994084+krsnadas04@users.noreply.github.com>
2022-08-22 15:19:16 +0530

    Add files via upload
    Learning For loop

---
+++ LearnFor.java
@@ -0,0 +1,20 @@
+package learning;
+
+public class LearnFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 50; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		System.out.println("****************");
+		for (int j = 0; j < 10; j++) {
+			System.out.println(j);
+			++j;
+
+		}
+
+	}
+}
